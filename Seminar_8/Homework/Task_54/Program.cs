f1a0a4e326d54591d67badfff44620d180018a07
Muxammadali <sherikjumayev1202@gmail.com>
2023-02-19 16:22:56 +0500

    Seminar-8 - Homework - Task_54

---
+++ Seminar_8/Homework/Task_54/Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+try
+{
+    int m = ReadInt("Введите m");
+    int n = ReadInt("Введите n");
+    int[,] array = Create2DArray(m, n);
+    Print2DArray(array);
+    Console.WriteLine("Результирующий массив");
+    int[,] resultArray = Change2DArrayRows(array);
+    Print2DArray(resultArray);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] Create2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-100, 100);
+        }
+    }
+
+    return array;
+}
+
+
+int[,] Change2DArrayRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int min = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = min;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+
+int ReadInt(string title)
+{
+    Console.WriteLine(title);
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParsed)
+    {
+        return (number);
+    }
+
+    throw new Exception("Введены не корректные символы");
+}
+
+
+
+
+
+
+
