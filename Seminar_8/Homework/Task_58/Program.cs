8ff788438a35bbf81ec051a1ff46b87de9c0150b
Muxammadali <sherikjumayev1202@gmail.com>
2023-02-24 23:20:29 +0500

    seminar-8 Homework-Task_58

---
+++ Seminar_8/Homework/Task_58/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+// находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+try
+{
+    int m1 = ReadInt("Введите m1");
+    int n1 = ReadInt("Введите n1");
+    int m2 = ReadInt("Введите m2");
+    int n2 = ReadInt("Введите n2");
+    int[,] array1 = Create2DArray(m1, n1);
+    Print2DArray(array1);
+    Console.WriteLine("2 матрица");
+    int[,] array2 = Create2DArray(m2, n2);
+
+    
+    Print2DArray(array2);
+    int[,] resultArray = FindMultiplicationOfMatrix(array1, array2);
+    Print2DArray(resultArray);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] Create2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+
+    return array;
+}
+
+
+int[,] FindMultiplicationOfMatrix(int[,] array1, int[,] array2)
+{
+    if (array1.GetLength(1) != array2.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+
+    int[,] resultArray = new int[array1.GetLength(0), array2.GetLength(1)];
+
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                resultArray[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return resultArray;
+}
+
+
+int ReadInt(string title)
+{
+    Console.WriteLine(title);
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParsed)
+    {
+        return (number);
+    }
+
+    throw new Exception("Введены не корректные символы");
+}
