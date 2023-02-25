45788edc8feb80c10f03d8631bee9ce3101358ac
Muxammadali <sherikjumayev1202@gmail.com>
2023-02-25 21:05:13 +0500

    seminar-8 Homework-Task_60

---
+++ Seminar_8/Homework/Task_60/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0, 1, 0)
+// 34(1, 0, 0) 41(1, 1, 0)
+// 27(0, 0, 1) 90(0, 1, 1)
+// 26(1, 0, 1) 55(1, 1, 1)
+
+int[,,] array = Create3DArray();
+
+Print3DArray(array);
+
+
+int[,,] Create3DArray()
+{
+    int[,,] array = new int[2,2,2];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                int number;
+                do
+                {
+                    number = random.Next(1, 101);
+                }
+                while (Containts(array, number));
+                array[i,j,k] = number;
+            }
+        }
+    }
+    return array;
+}
+
+
+bool Containts(int[,,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i,j,k] == number)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (int k = 0; k < array.GetLength(0); k++)
+    {
+        for (int i = 0; i < array.GetLength(2); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
